2e41950f197e674a14942232c6171c4801ab0963
Kanat25 <nazarovkanat7@gmail.com>
2022-04-13 21:40:39 +0300

    lesson3

---
+++ lesson3/script.js
@@ -0,0 +1,69 @@
+// function sayhello(){
+//     console.log("hello")
+// }
+//
+// sayhello();
+
+// function getfullname(user, age,gender) {
+//     console.log(user, age, gender)
+//
+// }
+// const user ={
+//     first_name:"alex",
+//     last_name:"joe"
+// }
+// getfullname(user, 18, "male")
+//
+// const anonym = function (user, age) {
+//     console.log("name ", user, age)
+// }
+// anonym("kana", 29);
+
+
+//
+//
+// function number(n1, n2) {
+//     let result=0;
+//     if (n1>n2)
+//
+
+// }
+
+
+function n(number1, number2) {
+    if (number1 > number2) {
+        console.log("число 1 больше");
+
+    } else if (number1 === number2) {
+        console.log("числа одинаковы");
+
+    } else {
+        console.log("число 2 больше");
+    }
+}
+
+n(7,6)
+
+// 2
+
+function twoMas(array1,array2) {
+
+    if (array1.length > array2.length){
+        console.log("массив 1 длиннее массива 2");
+
+    } else if (array1.length === array2.length) {
+        console.log("массивы одинаковы");
+
+    } else {
+        console.log("массив 2 больше");
+    }
+}
+twoMas([3], [2])
+
+// 3
+
+function countchar(user1) {
+    user1.length
+    console.log(user1.length)
+}
+countchar("fkwjfijeofihofh")
